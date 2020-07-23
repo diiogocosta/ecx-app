f7e1e5282b9598503b11ef6f8664ad9f69794ca0
Diogo da Costa <diiogocosta@hotmail.com>
2020-07-23 21:56:53 +0100

    changed onboarding illustrations

--- src/pages/onboarding/slides.js
+++ src/pages/onboarding/slides.js
@@ -1,26 +1,26 @@
 const slides = [
   {
-    imgSrc: require('../../../assets/images/trophy.png'),
+    imgSrc: require('../../../assets/images/slide-1.png'),
     title: 'Garanta o seu checkin',
     description:
       'Veja os horários e treinos disponíveis e garanta o seu check-in.',
     showButton: false,
   },
   {
-    imgSrc: require('../../../assets/images/trophy.png'),
+    imgSrc: require('../../../assets/images/slide-2.png'),
     title: 'Grave seus resultados',
     description:
       'Após o treino, grave seu resultado e veja no ranking, qual a sua posição :)',
     showButton: false,
   },
   {
-    imgSrc: require('../../../assets/images/trophy.png'),
+    imgSrc: require('../../../assets/images/slide-4.png'),
     title: 'Compare seus resultados',
     description: 'Compare seus resultados com seu amigo do Box',
     showButton: false,
   },
   {
-    imgSrc: require('../../../assets/images/trophy.png'),
+    imgSrc: require('../../../assets/images/slide-7.png'),
     title: 'Preencha seu perfil',
     description: 'Agora vamos preencher o seu perfil',
     showButton: true,
